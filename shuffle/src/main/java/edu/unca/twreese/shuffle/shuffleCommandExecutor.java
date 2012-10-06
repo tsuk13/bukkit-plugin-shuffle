d01b57aa4a656fa2830f9ff2b2c62dbbf28239f9
Travace Reese <tsuk13@gmail.com>
2012-10-06 00:56:35 -0400

    /shuffle shuf now teleports you to a random position at ground level within 500 blocks

--- shuffle/src/main/java/edu/unca/twreese/shuffle/shuffleCommandExecutor.java
+++ shuffle/src/main/java/edu/unca/twreese/shuffle/shuffleCommandExecutor.java
@@ -1,8 +1,12 @@
 package edu.unca.twreese.shuffle;
 
+import java.util.Random;
+
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.google.common.base.Joiner;
 
@@ -26,8 +30,15 @@ public class shuffleCommandExecutor implements CommandExecutor {
         if (args.length == 0){
         	return false;
         }
-        if (args[0].equalsIgnoreCase("shuf")){
+        if (args[0].equalsIgnoreCase("shuf") && sender instanceof Player){
         	sender.sendMessage("shuf was called again");
+        	Player player = (Player) sender;
+        	Location loc = player.getLocation();
+        	Random rnd = new Random();
+        	loc.setX(loc.getX() - 500 + (rnd.nextDouble() * 1000));
+        	loc.setZ(loc.getZ() - 500 + (rnd.nextDouble() * 1000));
+        	loc.setY(player.getWorld().getHighestBlockYAt(loc));
+        	player.teleport(loc);
         	return true;
         }
         return false;
